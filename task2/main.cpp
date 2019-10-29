2bf03f50ad142d04008b677dbda6a15a52d7b159
valentiay <valentiay@yandex.ru>
2019-10-30 02:05:37 +0300

    Task2: Cool implementation

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+
 struct Nil {
 };
 
 template<class Head, class Tail>
 struct TypeList {
 };
+
+/*----------------------------------------------------------------------------*/
 
 template<class List, class A>
 struct Append;
@@ -18,34 +21,96 @@ struct Append<TypeList<Head, Tail>, A> {
     typedef TypeList<Head, typename Append<Tail, A>::result> result;
 };
 
+/*----------------------------------------------------------------------------*/
+
 template<class A = Nil, class B = Nil, class C = Nil, class D = Nil>
 struct MakeTypeList {
-    typedef TypeList<A, typename MakeTypeList<B, C, D>::result > result;
+    typedef TypeList<A, typename MakeTypeList<B, C, D>::result> result;
 };
-template <>
-struct MakeTypeList <Nil, Nil> {
+template<>
+struct MakeTypeList<Nil, Nil> {
     typedef Nil result;
 };
+
+/*----------------------------------------------------------------------------*/
+
+template<int A, class Tail>
+struct Array;
+
+template<class Array>
+struct Increment;
+template<int Head, class Tail>
+struct Increment<Array<Head, Tail> > {
+    typedef Array<Head + 1, typename Increment<Tail>::result> result;
+};
+template<>
+struct Increment<Nil> {
+    typedef Nil result;
+};
+
+/*----------------------------------------------------------------------------*/
+
+template<class Array>
+struct Length;
+template<int Head, class Tail>
+struct Length<Array<Head, Tail> > {
+    enum {
+        result = Length<Tail>::result + 1
+    };
+};
+template<>
+struct Length<Nil> {
+    enum {
+        result = 0
+    };
+};
+
+/*----------------------------------------------------------------------------*/
+
+template<class Array, int Index>
+struct Get;
+template<int Head, class Tail, int Index>
+struct Get<Array<Head, Tail>, Index> {
+    enum {
+        result = (Index == 0) ? Head : Get<Tail, Index - 1>::result
+    };
+};
+
+/*----------------------------------------------------------------------------*/
 
 template<class List, class A>
 struct GetIndexOf;
 template<class A>
 struct GetIndexOf<Nil, A> {
-    enum {
-        result = -1
-    };
+    typedef Nil result;
 };
 template<class Tail, class A>
 struct GetIndexOf<TypeList<A, Tail>, A> {
-    enum {
-        result = 0
-    };
+    typedef Array<0, typename Increment<typename GetIndexOf<Tail, A>::result>::result> result;
 };
 template<class Head, class Tail, class A>
 struct GetIndexOf<TypeList<Head, Tail>, A> {
-    enum {
-        result = (GetIndexOf<Tail, A>::result == -1) ? -1 : GetIndexOf<Tail, A>::result + 1
-    };
+    typedef typename Increment<typename GetIndexOf<Tail, A>::result>::result result;
+};
+
+/*----------------------------------------------------------------------------*/
+
+template<class Array>
+struct PrintArray;
+
+template<int Head, class Tail>
+struct PrintArray<Array<Head, Tail> > {
+    static void apply() {
+        std::cout << Head << " :: ";
+        PrintArray<Tail>::apply();
+    }
+};
+
+template<>
+struct PrintArray<Nil> {
+    static void apply() {
+        std::cout << "Nil" << std::endl;
+    }
 };
 
 int main() {
@@ -53,13 +118,22 @@ int main() {
     typedef TypeList<double, list1> list2;
     typedef Append<list1, double>::result list3;
 
-    std::cout << GetIndexOf<list2, double>::result << std::endl;
-    std::cout << GetIndexOf<list3, double>::result << std::endl;
-    std::cout << std::endl;
+    std::cout << std::endl << "double in double :: int :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<list2, double>::result>::apply();
 
-    std::cout << GetIndexOf<MakeTypeList<int, int, int, int>::result, int>::result << std::endl;
-    std::cout << GetIndexOf<MakeTypeList<int, int, int, int>::result, double>::result << std::endl;
-    std::cout << GetIndexOf<MakeTypeList<int, double, int, double>::result, int>::result << std::endl;
-    std::cout << GetIndexOf<MakeTypeList<int, double, int, double>::result, double>::result << std::endl;
+    std::cout << std::endl << "double in int :: double :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<list3, double>::result>::apply();
+
+    std::cout << std::endl << "int in int :: int :: int :: int :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<MakeTypeList<int, int, int, int>::result, int>::result>::apply();
+
+    std::cout << std::endl << "double in int :: int :: int :: int :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<MakeTypeList<int, int, int, int>::result, double>::result>::apply();
+
+    std::cout << std::endl << "int in int :: double :: int :: double :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<MakeTypeList<int, double, int, double>::result, int>::result>::apply();
+
+    std::cout << std::endl << "double in int :: double :: int :: double :: Nil:" << std::endl;
+    PrintArray<GetIndexOf<MakeTypeList<int, double, int, double>::result, double>::result>::apply();
     return 0;
 }
